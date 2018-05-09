0bf9a6f583039d97cf7c5d1aa84f22956302f627
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-09 22:25:37 +0100

    Create dschema & added lodash

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 const PORT = 4000
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}))
 
 app.listen(PORT, () => {
   console.log(`NOW LISTENING FOR REQUESTS ON PORT: ${PORT}`)
