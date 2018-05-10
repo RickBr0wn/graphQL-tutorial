fa16a8e01b79d891941fd58690303a60b64ebaa0
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-10 12:44:30 +0100

    Placed queries in external file

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import { gql } from 'apollo-boost'
 import { graphql } from 'react-apollo'
-
-const getBooksQuery = gql`
-  {
-    books{
-      name
-      id
-    }
-  }
-`
+import { getBooksQuery } from '../Queries/Queries'
 
 class BookList extends React.Component{
   displayBooks(){
