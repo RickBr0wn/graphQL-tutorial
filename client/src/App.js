7d173ed9f780dad9166124fa237fedd17b4a076d
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-10 11:38:42 +0100

    Added BookList Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class App extends Component {
+// Components
+import BookList from './Components/BookList'
+
+class App extends React.Component {
   render() {
     return (
       <div id="main">
         <h1>Rick's Reading List</h1>
+        <BookList />
       </div>
     );
   }
