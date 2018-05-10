e5e56159af310534636968f1012d71dc883b7ee7
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-10 12:03:16 +0100

    Added CORS

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 const PORT = 4000
+
+// Allow cross origin requests
+app.use(cors())
 
 // Connect to mLab database
 mongoose.connect('mongodb://rick:Harry123@ds219040.mlab.com:19040/graphql-tut')
