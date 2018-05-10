9322d9121f25e5f193aed613aa3d9c333214a051
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-10 12:00:59 +0100

    Added getBooksQuery

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 // Components
 import BookList from './Components/BookList'
 
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 class App extends React.Component {
   render() {
     return (
-      <div id="main">
-        <h1>Rick's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Rick's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
